2af9414d5069caa957630d58b11d092106d6c5b0
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-07-30 08:15:18 -0500

    Primer Commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = " Luis ";
+            Console.WriteLine("Bien-venido" + nom + "a MY PROYECT");
+
+        }
+    }
+}
