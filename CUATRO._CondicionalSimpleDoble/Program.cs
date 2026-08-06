ec702dc29c4f12937fc831f70ad85f43111f2653
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-08-06 09:29:18 -0500

    Condicionales SIMPLE y DOBLE

---
+++ CUATRO._CondicionalSimpleDoble/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CUATRO._CondicionalSimpleDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Estructura de control Condicional Simple.
+
+            //Algoridmo que lee la edad del usuario y verifique que sea mayor de edad. Si es asi, que diga "Bienvenido a mi Sitio Wed"
+
+
+            /* Console.WriteLine("Ingresa tu edad: "); byte ed;
+             ed = Convert.ToByte( Console.ReadLine() );
+
+             if (ed >= 18)
+             {
+                 Console.WriteLine("Bienvenido a mi sitio Wed :)");
+             }
+
+            
+ */
+
+            // EJERCICIO 2.
+
+            /* Console.WriteLine("Ingresa tu nombre, papu: "); string name;
+             name = Console.ReadLine();
+
+             Console.WriteLine("Ingresa tu sueldo, papu: "); double salariun;
+             salariun = Convert.ToDouble(Console.ReadLine());
+
+             if (salariun > 3000 )
+             {
+                 Console.WriteLine(name + ", papu, paga tus impuestos");
+             }
+
+             else
+             {
+                 Console.WriteLine(name + ", no te pagan lo suficiente, papu");
+             }*/
+
+
+
+            // EJERCICIO 3.
+
+            /* Console.WriteLine("Ingresa tu edad: "); byte ed;
+             ed = Convert.ToByte( Console.ReadLine() );
+
+             if (ed >= 18)
+             {
+                 Console.WriteLine("Bienvenido a mi sitio Wed :)");
+             }
+
+             else
+             {
+                 Console.WriteLine("Largate de mi sitio Wed menor >:(");
+             }
+ */
+
+        }
+    }
+}
---
+++ CUATRO._CondicionalSimpleDoble/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("CUATRO._CondicionalSimpleDoble")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CUATRO._CondicionalSimpleDoble")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("05abef47-1d4e-498e-924a-3a7548e1a3a5")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
