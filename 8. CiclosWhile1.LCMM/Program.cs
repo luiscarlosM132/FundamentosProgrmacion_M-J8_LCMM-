af5b4e7bd0f130dec9e01df828920a6bf1b4d421
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-08-25 23:08:25 -0500

    8. Ciclos While 1

---
+++ 8. CiclosWhile1.LCMM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _8.CiclosWhile1.LCMM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados.
+
+           Console.WriteLine("Ingresa un número, positivo para seguir sumando, o negativo para finalizar y sumor el total de números ingreados: "); 
+            int num = 0; 
+            int suma = 0;
+            num = Convert.ToByte(Console.ReadLine());
+
+            while (num >= 0)
+            {
+                suma += num;
+                Console.WriteLine("Ingrese otro número entero positivo o un número negativo para finalizar.");
+                num = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("La suma total de los números ingresados es: " + suma);
+        }
+
+    }
+    
+}
+
