c0441d54d8dbbdacf7dd123db3aa5c15f3ed33f7
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-08-27 09:25:45 -0500

    8. ciclos WHILE 2 Lcmm

---
+++ 8.ciclosWHILE2.Lcmm/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numMcero = 0;
+            int nummcero = 0;
+            int numIcero = 0;
+
+            int cont = 0;
+
+            Console.WriteLine("Confirme la cantidad de números que va ingresar: ");
+            int cant = int.Parse(Console.ReadLine());
+
+            while (cont < cant)
+            {
+
+                Console.WriteLine($"Ingrese {cant} números enteros al azar: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > 0)
+                {
+                    numMcero++;
+                }
+                else if (num < 0)
+                {
+                    nummcero++;
+                }
+                else
+                {
+                    numIcero++;
+                }
+                cont++;
+            }
+
+
+            Console.WriteLine("---- RESULTADO: ----");
+            Console.WriteLine($"Cantidad de números enteros mayores que cero: {numMcero}");
+            Console.WriteLine($"Cantidad de números enteros menores que cero: {nummcero}");
+            Console.WriteLine($"Cantidad de números enteros iguales que cero: {numIcero}");
+
+        }
+    }
+}
