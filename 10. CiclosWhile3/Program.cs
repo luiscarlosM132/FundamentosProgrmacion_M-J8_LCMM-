deb2737b713407fbdbf9a6e4393a21567fe543dc
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-08-28 21:11:42 -0500

    10. Ciclos WHILE 3 LCMM

---
+++ 10. CiclosWhile3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace _10.CiclosWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numR = random.Next(1, 101);
+
+            int intoU = 0;
+            
+
+            Console.WriteLine("Adivina el un número de 1 hasta 100");
+
+           
+            while (intoU != numR)
+            {
+                Console.Write("Ingresa tu intento: ");
+
+               
+                if (int.TryParse(Console.ReadLine(), out intoU))
+                {
+                    
+
+                    if (intoU < numR)
+                    {
+                        Console.WriteLine("Demasiado bajo. Intenta de nuevo.");
+                    }
+
+                    else if (intoU > numR)
+                    {
+
+                        Console.WriteLine("Demasiado alto. Intenta de nuevo.");
+
+                    }
+
+                    else
+                    {
+
+                        Console.WriteLine($"¡Felicidades! ¡Adivinaste el número {numR}!");
+                       
+                    }
+                }
+
+                else
+                {
+
+                    Console.WriteLine("Por favor, ingresa un número entero válido.");
+
+                }
+            }
+        }
+    }
+}
