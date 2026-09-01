3c33fc5b87bd0adc27bb5277196b24679140bb99
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-09-01 09:28:04 -0500

    11. Ciclo Do While lcmm

---
+++ 11. CicloDoWhile/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+
+namespace _11.CicloDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*  int cont = 0;
+              int acum = 1;
+              do
+              {
+
+                  acum += cont;
+
+                  cont++;
+
+              }
+              while (cont <= 5);
+
+              Console.WriteLine($"La suma de los cinco primeros números es : {acum}");*/
+
+
+
+            /*int cont = 0;
+            int nume;
+            int result;
+            int selec;
+            do
+            {
+                Console.WriteLine("Ingresa el número que quieras multiplicar hasta 10: ");
+                nume = int.Parse(Console.ReadLine());
+
+                do
+                {
+                    
+
+                    result = nume * cont;
+
+                    Console.WriteLine($"{nume} x {cont} = {result}");
+
+                    cont++;
+
+
+                } while (cont <= 10);
+
+                Console.WriteLine("Desea continuar? 1 para si  //  2 para no: ");
+                selec = int.Parse(Console.ReadLine());
+
+            } while(selec == 1);
+
+            Console.WriteLine("Fin del programa, buen dia.");*/
+
+
+            // 3. Crear un algoritmo que permita calcular y mostrar por pantalla los números primos hasta el número 100.
+
+            int nume = 0;
+            int cont = 0;
+
+            do
+            {
+                do {
+
+
+
+
+                } while (cont == 100);
+
+
+            } while (cont == 100);
+
+
+
+        }
+    }
+}
