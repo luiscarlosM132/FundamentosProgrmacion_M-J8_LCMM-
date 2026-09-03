39878076856c9b03c99a3e697cc2f709cc684bfd
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-09-03 09:27:04 -0500

    13._Ciclo_For_Lcmm

---
+++ 13.CicloForLcmm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _13.CicloForLcmm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acum = 0;
+
+            for (int cont = 1; cont <= 5; cont++)
+            {
+                acum += cont;
+            }
+            Console.WriteLine($"La suma de los primeros cinco primeros números es: {acum}");
+        }
+    }
+}
