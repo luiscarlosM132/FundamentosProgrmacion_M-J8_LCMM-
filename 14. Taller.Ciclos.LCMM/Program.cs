88dc55c1bff1c5dfcaa2795b21f11ee4e275aa2c
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-09-08 09:33:33 -0500

    /14. Taller.Ciclos.LCMM/14.

---
+++ 14. Taller.Ciclos.LCMM/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+
+namespace _14.Taller.Ciclos.LCMM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  //////////////////////////1.
+            /*int cont = 0;
+            float nota = 0;
+            float notaSUM = 0;
+            int selec;
+            do
+            {
+                Console.WriteLine("Ingrese la nota: ");
+                nota = float.Parse(Console.ReadLine());
+
+
+                notaSUM += nota;
+
+
+                Console.WriteLine("Desea continuar? 1 para si  //  cualquier otro numero para no: ");
+                selec = int.Parse(Console.ReadLine());
+
+                cont++;
+
+            }
+            while (selec == 1);
+
+            Console.WriteLine($"El promedio es: {(notaSUM / cont), 2}");
+
+*/
+
+            ///////////////////////2.
+
+                 /*int numero;
+                 Console.Write("Ingrese un número: ");
+                 numero = int.Parse(Console.ReadLine());
+                 Console.WriteLine("Los divisores de {0} son:", numero);
+            for (int contador = 1; contador <= numero; contador++)
+            {
+                if (numero % contador == 0)
+                {
+                    Console.WriteLine(contador);
+                }
+            }*/
+
+                ///////////////////////3.
+                /*    string slec;
+                    do {
+
+                        int exponente;
+                        int bas;
+                        int potencia = 1;
+                        Console.WriteLine("Ingrese la base: ");
+                        bas = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el exponente: ");
+                        exponente = int.Parse(Console.ReadLine());
+
+                        for (int contador = 1; contador <= exponente; contador++)
+                        {
+                            Console.WriteLine($"{contador} -- {bas}");
+                            potencia *= bas;
+                        }
+                        Console.WriteLine($"Resultado: {potencia}");
+
+                        Console.WriteLine("Desea finalizar? n para si  //  cualquier otra tecla para no: ");
+                         slec = Console.ReadLine();
+
+                    } while (slec != "n");*/
+
+            //////////////////////////4.
+            
+
+
+
+
+        }
+    }
+}
