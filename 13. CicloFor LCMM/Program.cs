a0f4c9de60a6d91c1fdc854e125e13d7e4ca317f
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-09-07 19:47:06 -0500

    13. CicloFor LCMM

---
+++ 13. CicloFor LCMM/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _13.CicloFor_LCMM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese el número inicial del rango: ");
+            int numinicio = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número final del rango: ");
+            int numfin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Múltiplos de 5 entre {numinicio} y {numfin}:");
+
+            for (int i = numinicio; i <= numfin; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
