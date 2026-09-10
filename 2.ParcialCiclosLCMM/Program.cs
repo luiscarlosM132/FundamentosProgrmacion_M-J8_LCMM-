a7c6b2ec4a1a7e6fb192b84abdc61907f19ff094
luiscarlosM132 <luisc.mendoza@upb.edu.co>
2026-09-10 09:09:19 -0500

    2.ParcialCiclosLCMM/2

---
+++ 2.ParcialCiclosLCMM/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+
+namespace _2.ParcialCiclosLCMM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acum = 0;
+
+            float cantM = 0;
+            float cantH = 0;
+            float cantT = 0;
+            float cantE = 0;
+
+            string gene;
+            string ocu;
+
+            int cantMUJtra = 0;
+            int cantHOMest = 0;
+            do
+            {
+                Console.WriteLine("Usted Trabaja o Estudia?: T Trabajo // E Estudio");
+                ocu = Console.ReadLine();
+
+                if (ocu == "T")
+                {
+                    cantT++;
+                }
+                else if (ocu == "E")
+                {
+                    cantE++;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. Ingrese data valido.");
+                }
+
+
+                Console.WriteLine("Genro: H Hombre // M Mujer");
+                gene = Console.ReadLine();
+
+                if (gene == "H")
+                {
+                    cantH++;
+                }
+                else if (gene == "M")
+                {
+                    cantM++;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. Ingrese data valido.");
+                }
+
+                Console.WriteLine("\n--//--//--//--//--//--//--\n");
+
+                if (ocu == "T" && gene == "M")
+                {
+                    cantMUJtra++;
+                }
+
+                if (ocu == "E" && gene == "H")
+                {
+                    cantHOMest++;
+                }
+
+
+                acum++;
+
+            } while (acum < 15) ;
+
+
+            Console.WriteLine("\n--//--//--//-- REPORTE FINAL --//--//--//--\n");
+
+            Console.WriteLine($"\nEl procentaje de Estudiantes es: {(cantE / 15) * 100}%, Total de estos {cantE}");
+            Console.WriteLine($"El procentaje de Trabajadores es: {(cantT / 15) * 100}%, Total de estos {cantT}\n");
+
+            Console.WriteLine("\n--//--//--//--//--//--//--\n");
+
+            Console.WriteLine($"La cantidad de Mujeres que Trabaja es: {cantMUJtra}");
+            Console.WriteLine($"La cantidad de Hombres que Estudia es: {cantHOMest}");
+            
+        }
+    }
+}
---
+++ 2.ParcialCiclosLCMM/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("2.ParcialCiclosLCMM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2.ParcialCiclosLCMM")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("298bb04b-25fc-4baf-b32b-4037abc7d323")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
